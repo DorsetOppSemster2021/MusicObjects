8440efa0b6fd35a80d2792ec2317e4d852353bad
John Rowley <outsidethebox@eircom.net>
2021-09-22 16:35:02 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using MusicObjects.models;
+using System;
+
+namespace MusicObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Dorset Orchestra");
+
+            Guitar myGuitar = new();
+
+            Console.WriteLine(myGuitar.Play());
+        }
+    }
+}
